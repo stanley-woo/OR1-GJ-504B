d279e03c61c18b0cfa1564a8367bfb3386e872fd
Stanley Woo <46428542+stanley-woo@users.noreply.github.com>
2026-05-06 16:10:13 +0800

    Finished first draft of market data provider layer. Sanity test worked: only US enquities.

--- apps/web/lib/market-data/providers/twelve-data.ts
+++ apps/web/lib/market-data/providers/twelve-data.ts
@@ -6,10 +6,19 @@ import type {
   MarketSymbol,
 } from "../types";
 
+const HISTORY_RANGE_PARAMS : Record<HistoryRange, TwelveDataHistoryParams> = {
+    "1D": { interval: "5min", outputsize: "78" },
+    "1W": { interval: "30min", outputsize: "65" },
+    "1M": { interval: "1day", outputsize: "22" },
+    "3M": { interval: "1day", outputsize: "66" },
+    "1Y": { interval: "1week", outputsize: "52"},
+};
+
+
 type TwelveDataErrorResponse = {
-  code?: number;
-  message?: string;
-  status?: "error";
+  code: number;
+  message: string;
+  status: "error";
 };
 
 type TwelveDataSymbolSearchItem = {
@@ -30,46 +39,53 @@ type TwelveDataSymbolSearchResponse =
     }
   | TwelveDataErrorResponse;
 
-type TwelveDataQuoteResponse =
-  | {
-      symbol: string;
-      name: string;
-      exchange: string;
-      mic_code?: string;
-      currency?: string;
-      datetime?: string;
-      timestamp?: string;
-      open?: string;
-      high?: string;
-      low?: string;
-      close?: string;
-      volume?: string;
-      previous_close?: string;
-      change?: string;
-      percent_change?: string;
-      average_volume?: string;
-      is_market_open?: boolean;
-      fifty_two_week?: {
-        low?: string;
-        high?: string;
-        low_change?: string;
-        high_change?: string;
-        low_change_percent?: string;
-        high_change_percent?: string;
-        range?: string;
-      };
-      status?: "ok";
-    }
-  | TwelveDataErrorResponse;
+
+type TwelveDataQuoteSuccessResponse = {
+    symbol: string;
+    name: string;
+    exchange: string;
+    mic_code? : string;
+    currency : string;
+    datetime? : string;
+    timestamp? : string;
+    open?: string;
+    high?: string;
+    low?: string;
+    close: string;
+    volume?: string;
+    previous_close: string;
+    change: string;
+    percent_change: string;
+    average_volume?: string;
+    is_market_open?: boolean;
+    fifty_two_week?: {
+        low: string;
+        high: string;
+        low_change: string;
+        high_change: string;
+        low_change_percent: string;
+        high_change_percent: string;
+        range: string;
+    };
+    status: "ok";
+};
+
+
+type TwelveDataQuoteResponse = TwelveDataQuoteSuccessResponse | TwelveDataErrorResponse;
+
+type TwelveDataHistoryParams = {
+    interval: string;
+    outputsize: string;
+};
 
 type TwelveDataTimeSeriesMeta = {
   symbol: string;
   interval: string;
-  currency?: string;
-  exchange_timezone?: string;
-  exchange?: string;
-  mic_code?: string;
-  type?: string;
+  currency: string;
+  exchange_timezone: string;
+  exchange: string;
+  mic_code: string;
+  type: string;
 };
 
 type TwelveDataTimeSeriesValue = {
@@ -78,7 +94,7 @@ type TwelveDataTimeSeriesValue = {
   high: string;
   low: string;
   close: string;
-  volume?: string;
+  volume: string;
 };
 
 type TwelveDataTimeSeriesResponse =
@@ -136,15 +152,101 @@ export class TwelveDataProvider implements MarketDataProvider {
     return number;
   }
 
+  private mapSymbol(raw: TwelveDataSymbolSearchItem): MarketSymbol {
+    return {
+      symbol: raw.symbol,
+      name: raw.instrument_name,
+      exchange: raw.exchange,
+      currency: raw.currency ?? "USD",
+    }; 
+  }
+
+  private mapQuote(raw : TwelveDataQuoteSuccessResponse) : MarketQuote {
+    return {
+        symbol: raw.symbol,
+        name: raw.name,
+        exchange: raw.exchange,
+        currency: raw.currency,
+        price: this.parseNumber(raw.close, "price"),
+        previousClose: this.parseNumber(raw.previous_close, "previousClose"),
+        change: this.parseNumber(raw.change, "change"),
+        changePercent: this.parseNumber(raw.percent_change, "changePercent"),
+    };
+  }
+
+  private mapRange(range: HistoryRange) : TwelveDataHistoryParams {
+    return HISTORY_RANGE_PARAMS[range];
+  }
+
+  private mapCandle(raw: TwelveDataTimeSeriesValue) : MarketCandle {
+    return {
+        time: raw.datetime,
+        open: this.parseNumber(raw.open, "open"),
+        high: this.parseNumber(raw.high, "high"),
+        low: this.parseNumber(raw.low, "low"),
+        close: this.parseNumber(raw.close, "close"),
+        volume: this.parseNumber(raw.volume, "volume"),
+    };
+  }
+
+
+
   async searchSymbols(query: string): Promise<MarketSymbol[]> {
-    throw new Error("Not implemented");
+    const queryTrim = query.trim();
+    if (queryTrim === "") {
+      return [];
+    }
+    const data = await this.fetchJson<TwelveDataSymbolSearchResponse>("/symbol_search", { symbol: queryTrim });
+    
+    if (!("data" in data)) {
+      throw new Error(data.message ?? "Twelve Data symbol search failed.");
+    }
+
+    return data.data.map((item) => this.mapSymbol(item));
   }
 
   async getQuote(symbol: string): Promise<MarketQuote> {
-    throw new Error("Not implemented");
+    const symbol_up = symbol.trim().toUpperCase();
+
+    if (symbol_up === "") {
+        throw new Error("Invalid symbol string.");
+    }
+
+    const data = await this.fetchJson<TwelveDataQuoteResponse>("/quote", { 
+        symbol: symbol_up,
+        exchange: "NASDAQ",
+    });
+
+    if (!("symbol" in data)) {
+        throw new Error(data.message ?? "Twelve Data getQuote failed.");
+    }
+
+    return this.mapQuote(data);
   }
 
   async getHistory(symbol: string, range: HistoryRange): Promise<MarketCandle[]> {
-    throw new Error("Not implemented");
+    const symbol_up = symbol.trim().toUpperCase();
+    const query_range = this.mapRange(range);
+
+    if (symbol_up == "") {
+        throw new Error("Invalid symbol string.");
+    }
+
+    const data = await this.fetchJson<TwelveDataTimeSeriesResponse>("/time_series", 
+        { 
+            symbol: symbol_up,
+            interval: query_range.interval,
+            outputsize: query_range.outputsize,
+            exchange: "NASDAQ",
+        });
+
+    if (!("meta" in data)) {
+        throw new Error(data.message ?? "Twelve Data getHistory failed.");
+    }
+
+    const raw_data = data.values;
+
+    return raw_data.map((item) => this.mapCandle(item)).reverse();
+
   }
 }
